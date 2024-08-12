09e0cf71191ca39af0e77996a18146009d57cfd1
Punnoose Punnen <punnoose88@gmail.com>
2024-08-12 22:55:51 +0530

    deployment

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import uvicorn 
- 
-if __name__ == '__main__':
-    uvicorn.run(
-        app = 'app.main:app',
-        host = 'localhost',
-        port = 8000,
-        reload = True
-    )
-    
+from fastapi import FastAPI
+from routes.auth_routes import auth_router 
+from routes.user_route import user_router
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key ='OUTFITRR')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(auth_router)
+app.include_router(user_router)
+
