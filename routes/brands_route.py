fc5f6b7965b671625d5c4c42723a52ac9659722c
Punnoose Punnen <punnoose88@gmail.com>
2024-08-13 19:12:03 +0530

    product routes for feed and product screen

--- routes/brands_route.py
+++
@@ -1,7 +0,0 @@
-# from fastapi import APIRouter
-# from starlette.responses import JSONResponse
-# from config.database import brand_collection
-# from models.product_model import Product 
-# from bson import ObjectId
-
-# brand_router = APIRouter()
--- routes/product_routes.py
+++ routes/product_routes.py
@@ -3,21 +3,51 @@ from starlette.responses import JSONResponse
 from config.database import products_collection
 from models.product_model import Product 
 from bson import ObjectId
+import json
 
 product_router = APIRouter()
 
-@product_router.get('/allProducts')
-async def get_all_products():
-    products = products_collection.find()
-    return JSONResponse({'products': list(products)})
+@product_router.get('/search')
+async def search_products():
+    try:
+        products_cursor = products_collection.find({'sub_category': 'Oversized T-shirt'})
+        products_list = [serialize_product(product) for product in products_cursor]
+        if not products_list:
+            return JSONResponse({'message': 'No products found!'}, status_code=404)
+        return JSONResponse({'products': products_list}, status_code=200)
+    except Exception as e:
+        return JSONResponse({'message': str(e)}, status_code=500)
 
-@product_router.post('/newProduct')
-async def new_product(product: Product):
-    new_product = dict(product)
-    created_product = products_collection.insert_one(new_product)
-    if created_product:
-        return JSONResponse({'message': 'Product created!'})
-    return JSONResponse({'message': 'Product not created!'})
+@product_router.get('/feed')
+async def get_all_products():
+    try:
+        projection = {
+            'productName': 1,
+            'price': 1,         
+            'cover_image': 1,      
+            'brand': 1,            
+            'fit': 1,               
+            'sub_category': 1        
+        }
+        products_cursor = products_collection.find({}, projection)
+        products_list = [serialize_product(product) for product in products_cursor]
+        if not products_list:
+            return JSONResponse({'message': 'No products found!'}, status_code=404)
+        return JSONResponse({'products': products_list}, status_code=200)
+    except Exception as e:
+        return JSONResponse({'message': str(e)}, status_code=500)
+
+def serialize_product(product):
+    product['_id'] = str(product['_id'])
+    return product
+
+
+@product_router.post('/uploadProduct')
+async def new_product():
+    with open('routes/men_oversized_tshirt_details.json') as file:
+        products = json.load(file)
+    result = products_collection.insert_many(products)
+    print(f'{len(result.inserted_ids)} products were inserted.')
 
 @product_router.get('/getProduct/{id}')
 async def get_product(id: str):
@@ -25,26 +55,15 @@ async def get_product(id: str):
         object_id = ObjectId(id)
     except Exception as e:
         return JSONResponse({'message': 'Invalid product ID'}, status_code=400)
-    
     product = products_collection.find_one({'_id': object_id})
     if not product:
         return JSONResponse({'message': 'Product not found!'}, status_code=404)
+    product = serialize_product(product)
     return JSONResponse({'product': product})
 
 @product_router.get('/allBrands')
 async def get_all_brands():
     distinct_brands = products_collection.distinct('brand')
     return JSONResponse({'brands': distinct_brands})
-
-@product_router.get('/brandProducts/{brand}')
-async def get_brand_products(brand: str):
-    products = products_collection.find({'brand': brand})
-    return JSONResponse({'products': list(products)})
-
-@product_router.get('/masterCategories')
-async def get_master_categories():
-    distinct_master_categories = products_collection.distinct('master_category')
-    return JSONResponse({'master_categories': distinct_master_categories})
-
 
 
