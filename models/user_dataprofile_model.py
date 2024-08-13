9073398a3e0f02f7f0e2a248104b8cfbc029adc1
Punnoose Punnen <punnoose88@gmail.com>
2024-08-13 17:24:41 +0530

    user_dataprofile schema update

--- models/user_dataprofile_model.py
+++ models/user_dataprofile_model.py
@@ -1,18 +1,55 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional, Dict
+from pydantic import BaseModel, Field, validator, root_validator
+from typing import List, Optional, Dict, Union
+from enum import Enum
+
+
+class GenderEnum(str, Enum):
+    male = "male"
+    female = "female"
+    unisex = "unisex"
+
+class PriceEnum(str, Enum):
+    pc1 = "0-599"
+    pc2 = "600-1499"
+    pc3 = "1500-2499"
+    pc4 = "2500-3499"
+    pc5 = "3500-4499"
+    pc5 = "4500+"
+
+class AgeEnum(str, Enum):
+    ac1 = "18-25"
+    ac2 = "26-35"
+    ac3 = "36-45"
+    ac4 = "46+"
+
+class FitEnum(str, Enum):
+    fit1 = "Slim Fit"
+    fit2 = "True to size "
+    fit3 = "Oversized Fit"
+
+
+class MaleMeasurement(BaseModel):
+    chest: Optional[int] = None
+    waist: Optional[int] = None
+
+
+class FemaleMeasurement(BaseModel):
+    hip: Optional[int] = None
+    waist: Optional[int] = None
+    bust: Optional[int] = None
+
 
 class UserDataProfile(BaseModel):
     user_id: str
-    gender: str = None 
-    age_group: str = None 
-    location: str = None
+    gender: GenderEnum
+    age_group: AgeEnum
     preferred_brands: List[str] = Field(default_factory=list)
-    budget_range: str = None 
-    preferred_sizes: Dict[str, int] = Field(default_factory=dict)  
-    style_preferences: Optional[str] = None 
+    price_range: PriceEnum
+    measurements: Dict[str, Optional[Union[MaleMeasurement, FemaleMeasurement]]] = Field(default_factory=dict)
+    fit: FitEnum
+    location: Optional[str] = None
     collaborative_filter: Optional[bool] = False
-    preferred_fits: str=None
     preferred_themes: List[str] = Field(default_factory=list)
     preferred_master_categories: List[str] = Field(default_factory=list)
     preferred_sub_categories: List[str] = Field(default_factory=list)
-    brand_blacklist: Optional[List[str]] = Field(default_factory=list)  
+    brand_blacklist: Optional[List[str]] = Field(default_factory=list)
